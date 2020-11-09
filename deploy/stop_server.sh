e9f8af74d19b1d9f4e8a14b6be033f27b9bccf35
asadahyuma <asadahyuma@team-lab.com>
2020-11-09 17:08:14 +0900

    デプロイのテスト

--- deploy/stop_server.sh
+++ deploy/stop_server.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
-
-kill -9 $(lsof -t -i:80)
-pid=$(lsof -t :80)
-if [ $pid != null ]; then
-  kill $pid
-fi
+##!/bin/sh
+#
+#kill -9 $(lsof -t -i:80)
+#pid=$(lsof -t :80)
+#if [ $pid != null ]; then
+#  kill $pid
+#fi

1a835d8cab3e55d498bd04549651bdef213e30f3
asadahyuma <asadahyuma@team-lab.com>
2020-11-12 10:20:22 +0900

    シェルの修正

--- deploy/start_server.sh
+++ deploy/start_server.sh
@@ -2,4 +2,4 @@
 
 source /home/ec2-user/.bash_profile
 
-sudo systemctl restart api.service
+nohup java -jar -Dspring.profiles.active=production /home/ec2-user/asada-restapi-0.0.1-SNAPSHOT.jar 1>/dev/null 2>/dev/null &

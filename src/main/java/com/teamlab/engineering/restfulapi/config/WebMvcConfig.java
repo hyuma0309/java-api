822eecd482fa0f964aebe9498c8735966074ba5b
asadahyuma <asadahyuma@team-lab.com>
2020-11-25 22:34:41 +0900

    corsの修正

--- src/main/java/com/teamlab/engineering/restfulapi/config/WebMvcConfig.java
+++ src/main/java/com/teamlab/engineering/restfulapi/config/WebMvcConfig.java
@@ -29,7 +29,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
   public void addCorsMappings(CorsRegistry registry) {
     registry
         .addMapping("/**")
-        .allowedOrigins("http://localhost:3000/*")
+        .allowedOrigins("*")
         .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS");
   }
 }
